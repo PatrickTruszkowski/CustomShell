40e96626d8c4c0dfcc8cc9261a8cd05e436e14f2
Patrick Truszkowski <patricktruszkowski2005@gmail.com>
2026-04-09 13:01:50 -0400

    Began shell input

--- main.c
+++ main.c
@@ -1,4 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main()
 {
-    return 0;
+    char input[1024];
+
+    while (1)
+    {
+        printf("shell> ");
+
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = 0;
+
+        char* tokens[64];
+        int tokenCount = 0;
+
+        char* token = strtok(input, " ");
+
+        while (token != NULL)
+        {
+            tokens[tokenCount++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        if (tokenCount == 0)
+        {
+            continue;
+        }
+
+        char* command = tokens[0];
+
+        if (strcmp(command, "exit") == 0)
+        {
+            break;
+        }
+        else
+        {
+            printf("<Invalid Command>\n");
+        }
+    }
 }
